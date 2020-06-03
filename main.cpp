9a62de1c2ef90398c013f4e1c4a9d307c5fd5860
zunzzz <a0983208130@gmail.com>
2020-06-03 16:23:17 +0800

    MQTT send data complete

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "mbed_rpc.h"
 #include "fsl_port.h"
 #include "fsl_gpio.h"
+#include <math.h>
 #define UINT14_MAX        16383
 // FXOS8700CQ I2C address
 #define FXOS8700CQ_SLAVE_ADDR0 (0x1E<<1) // with pins SA0=0, SA1=0
@@ -30,6 +31,8 @@ RawSerial xbee(D12, D11);
 
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread t;
+EventQueue acc_queue(32 * EVENTS_EVENT_SIZE);
+Thread acc_thread;
 
 
 
@@ -37,62 +40,95 @@ void xbee_rx_interrupt(void);
 void xbee_rx(void);
 void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len);
 void FXOS8700CQ_writeRegs(uint8_t * data, int len);
-void GetAccelerometer(Arguments *in, Reply *out);
-RPCFunction rpcAcc(&GetAccelerometer, "GetAccelerometer");
+void GetVelocity(Arguments *in, Reply *out);
+RPCFunction rpcAcc(&GetVelocity, "GetVelocity");
 void reply_messange(char *xbee_reply, char *messange);
 void check_addr(char *xbee_reply, char *messenger);
-
+void Velocity();
+uint8_t data[2];
+int16_t acc16;
+float tt[3];
+uint8_t res[6];
+float v[30];
+int count_ = 0;
 int main(){
-  uint8_t data[2] ;
-  // Enable the FXOS8700Q
-  FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &data[1], 1);
-  data[1] |= 0x01;
-  data[0] = FXOS8700Q_CTRL_REG1;
-  FXOS8700CQ_writeRegs(data, 2);
-  pc.baud(9600);
+    // Enable the FXOS8700Q
+    FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &data[1], 1);
+    data[1] |= 0x01;
+    data[0] = FXOS8700Q_CTRL_REG1;
+    FXOS8700CQ_writeRegs(data, 2);
+    pc.baud(9600);
 
-  char xbee_reply[4];
+    char xbee_reply[4];
 
-  // XBee setting
-  xbee.baud(9600);
-  xbee.printf("+++");
-  xbee_reply[0] = xbee.getc();
-  xbee_reply[1] = xbee.getc();
-  if(xbee_reply[0] == 'O' && xbee_reply[1] == 'K'){
-    pc.printf("enter AT mode.\r\n");
-  }
-  xbee_reply[0] = '\0';
-  xbee_reply[1] = '\0';
-  xbee.printf("ATMY 0x222\r\n");
-  reply_messange(xbee_reply, "setting MY : 0x222");
+    // XBee setting
+    xbee.baud(9600);
+    xbee.printf("+++");
+    xbee_reply[0] = xbee.getc();
+    xbee_reply[1] = xbee.getc();
+    if(xbee_reply[0] == 'O' && xbee_reply[1] == 'K'){
+        pc.printf("enter AT mode.\r\n");
+    }
+    xbee_reply[0] = '\0';
+    xbee_reply[1] = '\0';
+    xbee.printf("ATMY 0x222\r\n");
+    reply_messange(xbee_reply, "setting MY : 0x222");
 
-  xbee.printf("ATDL 0x122\r\n");
-  reply_messange(xbee_reply, "setting DL : 0x122");
+    xbee.printf("ATDL 0x122\r\n");
+    reply_messange(xbee_reply, "setting DL : 0x122");
 
-  xbee.printf("ATID 0x1\r\n");
-  reply_messange(xbee_reply, "setting PAN ID : 0x1");
+    xbee.printf("ATID 0x1\r\n");
+    reply_messange(xbee_reply, "setting PAN ID : 0x1");
 
-  xbee.printf("ATWR\r\n");
-  reply_messange(xbee_reply, "write config");
+    xbee.printf("ATWR\r\n");
+    reply_messange(xbee_reply, "write config");
 
-  xbee.printf("ATMY\r\n");
-  check_addr(xbee_reply, "MY");
+    xbee.printf("ATMY\r\n");
+    check_addr(xbee_reply, "MY");
 
-  xbee.printf("ATDL\r\n");
-  check_addr(xbee_reply, "DL");
+    xbee.printf("ATDL\r\n");
+    check_addr(xbee_reply, "DL");
 
-  xbee.printf("ATCN\r\n");
-  reply_messange(xbee_reply, "exit AT mode");
-  xbee.getc();
+    xbee.printf("ATCN\r\n");
+    reply_messange(xbee_reply, "exit AT mode");
+    xbee.getc();
 
-  // start
-  pc.printf("start\r\n");
-  t.start(callback(&queue, &EventQueue::dispatch_forever));
-
-  // Setup a serial interrupt function of receiving data from xbee
-  xbee.attach(xbee_rx_interrupt, Serial::RxIrq);
+    // start
+    pc.printf("start\r\n");
+    t.start(callback(&queue, &EventQueue::dispatch_forever));
+    acc_thread.start(callback(&acc_queue, &EventQueue::dispatch_forever));
+    acc_queue.call(Velocity);
+    // Setup a serial interrupt function of receiving data from xbee
+    xbee.attach(xbee_rx_interrupt, Serial::RxIrq);
 }
+void Velocity()
+{
+    while(count_ < 30) {
+        float interval = 0.1;
+        float a = 0;
+        FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
 
+        acc16 = (res[0] << 6) | (res[1] >> 2);
+        if (acc16 > UINT14_MAX/2)
+            acc16 -= UINT14_MAX;
+        tt[0] = ((float)acc16) / 4096.0f;
+
+        acc16 = (res[2] << 6) | (res[3] >> 2);
+        if (acc16 > UINT14_MAX/2)
+            acc16 -= UINT14_MAX;
+        tt[1] = ((float)acc16) / 4096.0f;
+
+        acc16 = (res[4] << 6) | (res[5] >> 2);
+        if (acc16 > UINT14_MAX/2)
+            acc16 -= UINT14_MAX;
+        tt[2] = ((float)acc16) / 4096.0f;
+        
+        a = sqrt(tt[0] * tt[0] + tt[1] * tt[1]);
+        v[count_] = a * interval;
+        count_++;
+        wait(interval);
+    }
+}
 void xbee_rx_interrupt(void)
 {
   xbee.attach(NULL, Serial::RxIrq); // detach interrupt
@@ -142,32 +178,12 @@ void check_addr(char *xbee_reply, char *messenger){
   xbee_reply[2] = '\0';
   xbee_reply[3] = '\0';
 }
-void GetAccelerometer(Arguments *in, Reply *out) {
-   int16_t acc16;
-   float t[3];
-   uint8_t res[6];
-   FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
-
-   acc16 = (res[0] << 6) | (res[1] >> 2);
-   if (acc16 > UINT14_MAX/2)
-      acc16 -= UINT14_MAX;
-   t[0] = ((float)acc16) / 4096.0f;
-
-   acc16 = (res[2] << 6) | (res[3] >> 2);
-   if (acc16 > UINT14_MAX/2)
-      acc16 -= UINT14_MAX;
-   t[1] = ((float)acc16) / 4096.0f;
-
-   acc16 = (res[4] << 6) | (res[5] >> 2);
-   if (acc16 > UINT14_MAX/2)
-      acc16 -= UINT14_MAX;
-   t[2] = ((float)acc16) / 4096.0f;
-
-   pc.printf("FXOS8700Q ACC: X=%1.4f(%x%x) Y=%1.4f(%x%x) Z=%1.4f(%x%x)\r\n",\
-         t[0], res[0], res[1],\
-         t[1], res[2], res[3],\
-         t[2], res[4], res[5]\
-   );
+void GetVelocity(Arguments *in, Reply *out) {
+    pc.printf("Here\r\n");
+    for(int i = 0; i < 30; i++) {
+        pc.printf("v[%d] = %f\r\n", i, v[i]);
+        xbee.printf("%f\r\n", v[i]);
+    }
 }
 void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
    char t = addr;
---
+++ publish.py
@@ -0,0 +1,97 @@
+import paho.mqtt.client as paho
+import serial
+import time
+import numpy as np
+# XBee setting
+serdev = '/dev/ttyUSB0'
+s = serial.Serial(serdev, 9600)
+
+s.write("+++".encode())
+char = s.read(2)
+print("Enter AT mode.")
+print(char.decode())
+
+s.write("ATMY 0x122\r\n".encode())
+char = s.read(3)
+print("Set MY 0x122.")
+print(char.decode())
+
+s.write("ATDL 0x222\r\n".encode())
+char = s.read(3)
+print("Set DL 0x222.")
+print(char.decode())
+
+s.write("ATID 0x1\r\n".encode())
+char = s.read(3)
+print("Set PAN ID 0x1.")
+print(char.decode())
+
+s.write("ATWR\r\n".encode())
+char = s.read(3)
+print("Write config.")
+print(char.decode())
+
+s.write("ATMY\r\n".encode())
+char = s.read(4)
+print("MY :")
+print(char.decode())
+
+s.write("ATDL\r\n".encode())
+char = s.read(4)
+print("DL : ")
+print(char.decode())
+
+s.write("ATCN\r\n".encode())
+char = s.read(3)
+print("Exit AT mode.")
+print(char.decode())
+
+print("start sending RPC")
+v = np.ones(30)
+# send RPC to remote
+s.write("/GetVelocity/run\r".encode())
+for i in range(30):
+    velocity = float(s.readline())
+    print(velocity)
+    v.append(velocity)
+
+# MQTT
+mqttc = paho.Client()
+
+host = "192.168.43.207"
+topic = "velocity"
+
+# Callbacks
+def on_connect(self, mosq, obj, rc):
+      print("Connected rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+      print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+      print("Subscribed OK")
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+      print("Unsubscribed OK")
+
+# Set callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+# Connect and subscribe
+print("Connecting to " + host + "/" + topic)
+mqttc.connect(host, port=1883, keepalive=60)
+mqttc.subscribe(topic, 0)
+
+# Publish messages from Python
+num = 0
+while num != 30:
+      ret = mqttc.publish(topic, str(v[num]) + "\n", qos=0)
+      if (ret[0] != 0):
+            print("Publish failed")
+      mqttc.loop()
+      time.sleep(0.5)
+      num += 1
+s.close()
